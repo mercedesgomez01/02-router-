d8738d9613cae93f2de62e50d341ea727dcbb36f
mercedesgomez01 <mercedesgomez01@gmail.com>
2017-03-28 17:08:24 -0400

    added router and route in REACTDOM

--- start/browser/react/index.js
+++ start/browser/react/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+//To start using react-router, all we need to do is import the components we
+//need into our root js file
+import { Router, Route, hashHistory } from 'react-router';
+
 
 ReactDOM.render(
-  <AppContainer />,
+  <Router history={hashHistory}>
+    <Route path="/" component={AppContainer}>
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
